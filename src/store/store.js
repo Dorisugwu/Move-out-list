3aceec7e2151f1374d476ecbb1fe82c5becd3ebd
Doris <ugwudoris00@gmail.com>
2022-10-16 23:48:43 +0100

    move out list

---
+++ src/store/store.js
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+import RoomReducer from "./reducer/roomReducer";
+export default configureStore({
+    reducer: {
+        room: RoomReducer
+    }
+})
